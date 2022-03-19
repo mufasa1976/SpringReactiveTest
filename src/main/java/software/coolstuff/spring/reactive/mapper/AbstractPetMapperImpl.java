2c4211464d72c91c63b5544b6d8316d522b3ea75
mufasa1976 <mufasa1976@coolstuff.software>
2022-03-19 20:37:24 +0100

    set AbstractPetMapperImpl.populateEntityWithValuesOfResource() to final

--- src/main/java/software/coolstuff/spring/reactive/mapper/AbstractPetMapperImpl.java
+++ src/main/java/software/coolstuff/spring/reactive/mapper/AbstractPetMapperImpl.java
@@ -9,7 +9,7 @@ import software.coolstuff.spring.reactive.model.PetModel;
 @Mapper(implementationName = "PetMapperImpl")
 public abstract class AbstractPetMapperImpl implements PetMapper {
   @Override
-  public PetEntity populateEntityWithValuesOfResource(Tuple2<PetEntity, PetModel> tuple2Mono) {
+  public final PetEntity populateEntityWithValuesOfResource(Tuple2<PetEntity, PetModel> tuple2Mono) {
     final var entity = tuple2Mono.getT1();
     populateEntityWithValuesOfResource(entity, tuple2Mono.getT2());
     return entity;
