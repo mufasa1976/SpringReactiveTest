efba04e8b679a29f4fe2dfc3e361877a319f0be6
mufasa1976 <mufasa1976@coolstuff.software>
2022-03-20 18:26:27 +0100

    Added a repeatable 'Hello World!'

--- src/main/java/software/coolstuff/spring/reactive/controller/HelloWorldController.java
+++ src/main/java/software/coolstuff/spring/reactive/controller/HelloWorldController.java
@@ -1,9 +1,14 @@
 package software.coolstuff.spring.reactive.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @RestController
 @RequestMapping(HelloWorldController.ENDPOINT)
@@ -13,5 +18,12 @@ public class HelloWorldController {
   @GetMapping
   public Mono<String> helloWorld() {
     return Mono.just("Hello World!");
+  }
+
+  @GetMapping(params = "repeat", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+  public Flux<String> helloWorldRepeated(@RequestParam(name = "waitSeconds", defaultValue = "1") long waitSeconds) {
+    return Flux.just("Hello World!")
+               .repeat()
+               .delayElements(Duration.ofSeconds(waitSeconds));
   }
 }
--- src/test/java/software/coolstuff/spring/reactive/HelloWorldControllerTest.java
+++ src/test/java/software/coolstuff/spring/reactive/HelloWorldControllerTest.java
@@ -9,8 +9,11 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.util.UriComponentsBuilder;
 import software.coolstuff.spring.reactive.config.WebfluxConfiguration;
 import software.coolstuff.spring.reactive.controller.HelloWorldController;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @WebFluxTest(controllers = HelloWorldController.class)
@@ -29,5 +32,25 @@ class HelloWorldControllerTest {
              .expectStatus().isOk()
              .expectHeader().contentType(TEXT_PLAIN_UTF8)
              .expectBody(String.class).isEqualTo("Hello World!");
+  }
+
+  @Test
+  @DisplayName("GET /" + HelloWorldController.ENDPOINT + "/repeat?waitSeconds=0 and take 3 Events and expect 3 times 'Hello World!'")
+  void helloWorldRepeated() {
+    final var response =
+        webClient.get().uri(UriComponentsBuilder.fromPath("/" + HelloWorldController.ENDPOINT)
+                                                .query("repeat")
+                                                .queryParam("waitSeconds", "0")
+                                                .toUriString())
+                 .exchange()
+                 .expectStatus().isOk()
+                 .returnResult(String.class)
+                 .getResponseBody()
+                 .take(3)
+                 .collectList()
+                 .block();
+    assertThat(response)
+        .hasSize(3)
+        .containsOnly("Hello World!");
   }
 }
